0d69f75965916928898f779a71baf3538b037c50
KoutaKawase <suhrkawase@gmail.com>
2020-07-05 22:47:13 +0900

    feat: 🎸 渡されたファイル名の存在チェックを実装

--- Dog.Tests/FilesTest.cs
+++ Dog.Tests/FilesTest.cs
@@ -1,8 +1,40 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using Dog;
 
 namespace Dog.Tests
 {
     public class FilesTest
     {
+        private static readonly String _testDir = AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.IndexOf("bin"));
+        private static readonly String _fixturesPath = Path.Combine(_testDir, "fixtures");
+
+        [Test]
+        public void 存在しないファイルがあればfalseを返す()
+        {
+            var hoge = Path.Combine(_fixturesPath, "hoge.cs");
+            var fuga = Path.Combine(_fixturesPath, "fuga.json");
+            var none = Path.Combine(_fixturesPath, "none.txt");
+            var files = new Files(new List<String>(new[] { hoge, fuga, none }));
+            var result = files.Exists();
+
+            Assert.False(result);
+        }
+
+        [Test]
+        public void 全て存在するファイルならtrueを返す()
+        {
+            var hoge = Path.Combine(_fixturesPath, "hoge.cs");
+            var files = new Files(new List<String>(new[] { hoge }));
+            var result = files.Exists();
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void 渡された名前にディレクトリが存在すれば例外()
+        { }
     }
 }
--- Dog/Files.cs
+++ Dog/Files.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
 namespace Dog
 {
@@ -11,6 +13,16 @@ namespace Dog
         public Files(List<String> files)
         {
             _files = files;
+        }
+
+        //全てのファイル名が実在するものか検査するためのもの
+        public bool Exists()
+        {
+            var exists = _files.All(file =>
+            {
+                return File.Exists(file);
+            });
+            return exists;
         }
     }
 }
